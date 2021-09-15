e824ce39af881826c0180b4b0737109b54d28857
digarTTN <90311202+digarTTN@users.noreply.github.com>
2021-09-16 01:43:06 +0530

    Add files via upload

---
+++ employee.js
@@ -0,0 +1,13 @@
+let obj1 = [
+    {name:'Rajesh',age:25,salary:30000,DOB:08/02/1996},
+    {name: 'Anjali',age:30,salary:45000,DOB:06/03/1991},
+    {name:'Pranjal',age:32,salary:50000,DOB:02/06/1989},
+    {name:'Ramesh',age:46,salary:60000,DOB:03/01/1975},
+    {name:'Radhika',age:28,salary:35000,DOB:09/09/1993}
+         ]
+
+for(let i=0;i<obj1.length;i++)
+  if(obj1.salary>5000)
+{
+   console.log(name,age,DOB);
+} 
